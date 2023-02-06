072ec7d919b5b3b52a16affd38bd0832dda33678
caiorodias <caiorodias@hotmail.com>
2023-02-06 15:43:08 -0300

    métodos string

---
+++ metodos_string.py
@@ -0,0 +1,23 @@
+curso = '     Python    '
+
+print(curso.upper())
+
+print(curso.lower())
+
+print(curso.title())
+
+#tira os espaços da direita e esquerda
+print(curso.strip())
+
+#Tira os espaços da esquerda
+print(curso .lstrip())
+
+#Tira os espaços da direita
+print(curso.rstrip())
+
+curso2 = 'Python'
+
+#coloca o caracter escolhido até que chegue na quantidade de caracter desejada
+print(curso2.center(10,'#'))
+
+print('.'.join(curso2))
