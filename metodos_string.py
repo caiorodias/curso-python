d6ffd787f4227aba90aacc19603614f9d2c4a85a
Caio Dias <93464556+caiorodias@users.noreply.github.com>
2023-02-06 20:03:40 -0300

    Concluindo primeiro módulo de Python

--- metodos_string.py
+++ metodos_string.py
@@ -21,3 +21,63 @@ curso2 = 'Python'
 print(curso2.center(10,'#'))
 
 print('.'.join(curso2))
+
+# Formatar strings com f-string
+
+pi = 3.14159
+
+#print(f'Valor de PI: {pi:tamanho.casas apos virgulaf}')
+
+print(f'Valor de PI: {pi:.2f}')
+
+print(f'Valor de PI: {pi:10.2f}')
+
+# Fatiamento de String
+
+nome = 'Caio Rodrigo Dias da Silva'
+
+print(nome[0])
+# Primeira letra
+
+print(nome[-1])
+# Última letra
+
+print(nome[:4])
+# Do inicio ao 4 caractere
+
+print(nome[13:])
+# Do 13º caractere ao último
+
+print(nome[13:17])
+#do 13º ao 17º
+
+print(nome[13:17:2])
+# Do 13º ao 17º pulando 2 casas
+
+print(nome[:: -1])
+# Espelhar
+
+# Strings triplas
+
+name = 'Caio'
+
+mensagem = f'''
+    Olá, meu nome é {name},
+Eu estou aprendendo Python.
+        Essa mensagem tem diferentes recuos.
+'''
+
+# Com 3 aspas''' a string mantém os recuos.
+
+print('''
+      ============= Menu ============
+      
+      1 - Depositar
+      2 - Sacar
+      0 - Sair
+      
+      ================================
+      
+        Obrigado por usar nosso sistema!
+'''
+      )
