ae7c0477ed4d0893f4057cc26a3f74dec5680c97
Caio Dias <93464556+caiorodias@users.noreply.github.com>
2023-02-09 22:35:15 -0300

    tuplas

---
+++ tupla.py
@@ -0,0 +1,9 @@
+# Tupla é uma lista imutável
+
+cores = ('vermelho', 'azul', 'verde', 'azul',)
+
+#cores[0] = 'roxo' #TypeError: 'tuple' object does not support item assignment
+
+# Métodos disponíveis para tupla:
+
+# Count, Index e Len
